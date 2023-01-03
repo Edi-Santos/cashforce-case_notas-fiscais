0ae0dc9273b2867c18833607d243a28ef9c8d491
Ed Santos <edsantos2822@gmail.com>
2023-01-03 08:07:22 -0300

    fix:(server.js): corrigindo erro de desestruturação

--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-const { PORT } = process.env || 3002;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => console.log(`App rodando na porta ${PORT}`));
