1d5bdcdbeb759e4e4d034b584c4663baddb42752
Ed Santos <edsantos2822@gmail.com>
2023-01-04 21:31:21 -0300

    build:(Home.jsx): implementando a página <Home />

---
+++ front-end/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Header from '../components/home/header/Header';
+import Main from '../components/home/main/Main';
+import Menu from '../components/home/menu/Menu';
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Menu />
+      <Main />
+    </>
+  );
+}
+
+export default Home;
